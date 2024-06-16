6e04a3ce7aeb914e2332ff86ee1902f07684ad7c
Rudraksh Girdhar <rudrakshgirdhar9192@gmail.com>
2024-06-16 19:05:46 +0530

    12

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,9 @@
+import { config } from 'dotenv';
 import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
 import jobRouter from "./routes/jobRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import applicationRouter from "./routes/applicationRoutes.js";
-import { config } from "dotenv";
 import cors from "cors";
 import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
@@ -14,11 +14,12 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "DELETE", "PUT"],
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true,
   })
 );
+
 
 app.use(cookieParser());
 app.use(express.json());
@@ -34,6 +35,7 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
 dbConnection();
+
 
 app.use(errorMiddleware);
 export default app;
