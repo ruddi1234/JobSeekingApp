c77086f9131ceff766e1aee2403e3e5d270095ff
Rudraksh Girdhar <rudrakshgirdhar9192@gmail.com>
2024-06-16 16:15:38 +0530

    monog

--- Backend/database/dbConnection.js
+++ Backend/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGOURI, {
+    .connect(process.env.MONGO_URI, {
       dbName: "MERN_JOB_SEEKING_WEBAPP",
     })
     .then(() => {
